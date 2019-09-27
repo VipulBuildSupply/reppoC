5d6ec413dc72f0bf82ee13c191272af7da558a27
Sachin Sharma <trulysachin@gmail.com>
2019-09-27 18:19:46 +0530

    build fix

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+ga: "",
+production: false,
+debug_mode: false,
+apiURL: (window.location.host.split('.').length<3) ? (window.location.protocol+'//api.'+window.location.host) : (window.location.protocol+'//api.'+window.location.host.split('.')[window.location.host.split('.').length-2]+'.'+window.location.host.split('.')[window.location.host.split('.').length-1])
+};
+
--- src/environments/environment.stag.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    debug_mode: false,       // for debug logs
-    api_url:'http://sabho.ga/',
-};
