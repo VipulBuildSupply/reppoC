d85eb835a206be67d686589af3956ccee86432da
Priya Garg <priya.garg@buildsupply.com>
2019-12-09 15:30:23 +0530

    updated PO Model

--- src/app/shared/models/purchase-orders.ts
+++ src/app/shared/models/purchase-orders.ts
@@ -29,6 +29,7 @@ export class PurchaseOrders extends POList{
     totalItem?: number;
     totalItemAmount?: number;
     transporterTermCondition?: string;
+    poAddress: POAddress;
 }
 
 export class OrderItemsList extends POList{ 
@@ -59,4 +60,48 @@ export class DownloadPO{
     orginalFileName: string;
     servingUrl?: string;
     signedUrl: string;
+}
+
+export class POAddress{
+    billingAddress: Addresses;
+    deliveryAddress: Addresses;
+    sellerAddress: Addresses;
+}
+
+export class Addresses{
+    addressLine1: string;
+    addressLine2: string;
+    addressProofPhoto?: string;
+    addressTypeCode: string;
+    city: string;
+    cityCd: string;
+    cityId: number;
+    cityDto: CityInfo;
+    companyId?: number;
+    countryCd?: number;
+    countryId?: number
+    default: boolean;
+    deliveryRange?: number;
+    gstin: string;
+    id: number;
+    modifyDt: string;
+    name: string;
+    phoneNo: string;
+    pincode: string;
+    state: string;
+    stateCd: string;
+    stateDto: any;
+    stateId: StateInfo;
+}
+
+export class StateInfo{
+    id: number;
+    name: string;
+    code: string;
+    countryId?: number;
+}
+
+export class CityInfo extends StateInfo{
+    stateCode: string;
+    stateId: number;
 }
