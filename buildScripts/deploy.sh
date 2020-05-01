014cfc60583578da97f250b4eb3c52117420ebf3
Keshav-Uipl-at-645189432027 <keshavt@umbrellainfocare.com>
2020-05-01 12:20:10 +0530

    updated helm path in deploy.sh

--- buildScripts/deploy.sh
+++ buildScripts/deploy.sh
@@ -12,8 +12,12 @@ else
 fi
 echo $TAG
 
-ls helm/commerce-seller/
+rm -rf /home/jenkins/helm/
+
+#clone helm charts from repo
+git clone git@code.buildsupply.com:devops/helm.git /home/jenkins/helm/
+ls /home/jenkins/helm/dev/commerce-seller/
 #updating kubeconfig
 kubectl config use-context arn:aws:eks:ap-south-1:317596419736:cluster/dev-ecom-im-cluster
 #deploy on kubernetes using helm
-#helm upgrade commerce-seller helm/commerce-seller/ --set=deployment.image.tag=$TAG -n dev
+#helm upgrade commerce-seller /home/jenkins/helm/dev/commerce-seller/ --set=deployment.image.tag=$TAG -n dev
