f55ea48a09d69ab535a9ce7a60a1abafcd3e26e9
vipul <vipul.sharma@buildsupply.com>
2020-05-04 17:55:51 +0530

    fix

--- buildScripts/envsetup.sh
+++ buildScripts/envsetup.sh
@@ -1,7 +1,7 @@
 set -x
 
 echo "" > /usr/share/nginx/html/assets/env.js
-echo "(function(window) {window.env.api_url = '"$BUILDSUPPLY_COMMERCE_BASE_API_URL"'" >> /usr/share/nginx/html/assets/env.js
-echo "window.env.payment_api_url ='"$BUILDSUPPLY_COMMERCE_PAYMENT_API_URL"'" >> /usr/share/nginx/html/assets/env.js 
+echo "(function(window) {window.env = {}; window.env.api_url = '"$BUILDSUPPLY_COMMERCE_BASE_API_URL"';" >> /usr/share/nginx/html/assets/env.js
+echo "window.env.payment_api_url ='"$BUILDSUPPLY_COMMERCE_PAYMENT_API_URL"';" >> /usr/share/nginx/html/assets/env.js 
 echo "})(this);"  >> /usr/share/nginx/html/assets/env.js
 
