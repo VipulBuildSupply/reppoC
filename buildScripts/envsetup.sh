447b7aed5c17bcb7e86e67ccad2bdc7dd5ede754
vipul <vipul.sharma@buildsupply.com>
2020-05-05 19:31:02 +0530

    fixes

--- buildScripts/envsetup.sh
+++ buildScripts/envsetup.sh
@@ -1,7 +1,17 @@
 set -x
 
 echo "" > /usr/share/nginx/html/assets/env.js
-echo "(function(window) {window.env.api_url = '"$BUILDSUPPLY_COMMERCE_BASE_API_URL"'" >> /usr/share/nginx/html/assets/env.js
+echo "(function(window) {window.env = {}; " >> /usr/share/nginx/html/assets/env.js
+if [ $BUILDSUPPLY_COMMERCE_BASE_API_URL ]
+then
+  echo '"window.env.api_url" : "'$BUILDSUPPLY_COMMERCE_BASE_API_URL'",' >> /opt/buyer/dist/browser/assets/env.json
+else
+echo '"window.env.api_url" : "https://api.yeho.ga/",' >> /opt/buyer/dist/browser/assets/env.json
+fi
+
+
 echo "window.env.payment_api_url ='"$BUILDSUPPLY_COMMERCE_PAYMENT_API_URL"'" >> /usr/share/nginx/html/assets/env.js 
 echo "})(this);"  >> /usr/share/nginx/html/assets/env.js
+echo "const oppo = {}; oppo.env.api_url = '"$BUILDSUPPLY_COMMERCE_BASE_API_URL"';" >> /usr/share/nginx/html/assets/env.js
+echo "oppo.env.payment_api_url ='"$BUILDSUPPLY_COMMERCE_PAYMENT_API_URL"'" >> /usr/share/nginx/html/assets/env.js
 
