18a3d21124a4ec160edd8e88836a142dc7c01077
Satish Kumar Khowal <satish.khowal@buildsupply.com>
2020-05-06 09:31:52 +0000

    Update envsetup.sh

--- buildScripts/envsetup.sh
+++ buildScripts/envsetup.sh
@@ -1,8 +1,8 @@
 set -x
 
-echo "" > /opt/buyer/dist/browser/assets/env.js
-echo "(function(window) {window.env = {};window.env.api_url = '"$BUILDSUPPLY_COMMERCE_BASE_API_URL"';" >> /opt/buyer/dist/browser/assets/env.js
-echo "window.env.payment_api_url ='"$BUILDSUPPLY_COMMERCE_PAYMENT_API_URL"';" >> /opt/buyer/dist/browser/assets/env.js 
-echo "})(this);"  >> /opt/buyer/dist/browser/assets/env.js
+echo "" > /usr/share/nginx/html/assets/env.js
+echo "(function(window) {window.env = {};window.env.api_url = '"$BUILDSUPPLY_COMMERCE_BASE_API_URL"';" >> /usr/share/nginx/html/assets/env.js
+echo "window.env.payment_api_url ='"$BUILDSUPPLY_COMMERCE_PAYMENT_API_URL"';" >> /usr/share/nginx/html/assets/env.js
+echo "})(this);"  >> /usr/share/nginx/html/assets/env.js
 
 WORKDIR /usr/share/nginx/html/
